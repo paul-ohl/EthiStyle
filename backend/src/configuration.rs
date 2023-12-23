071bc637c086eb9732abc70df5828a0f89a12c83
Paul OHL <paul.lv.ohl@gmail.com>
2023-12-23 16:24:23 +0100

    feat: Added Dockerfile creation and used mold!

--- backend/src/configuration.rs
+++ backend/src/configuration.rs
@@ -1,10 +1,62 @@
 use config::File;
 use secrecy::{ExposeSecret, Secret};
 
+pub fn get_config() -> Result<Settings, config::ConfigError> {
+    let config = config::Config::builder();
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+
+    let configuration_dir = base_path.join("configuration");
+    let config = config.add_source(File::from(configuration_dir.join("base")).required(true));
+
+    let app_env: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "dev".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT");
+
+    let config = config
+        .add_source(File::from(configuration_dir.join(app_env.as_str())).required(true))
+        .build()?;
+    config.try_deserialize()
+}
+
+pub enum Environment {
+    Dev,
+    Prod,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Dev => "dev",
+            Environment::Prod => "prod",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "dev" | "local" | "development" => Ok(Self::Dev),
+            "prod" | "production" => Ok(Self::Prod),
+            other => Err(format!(
+                "{} is not a supported environment. Use either `dev` or `prod`.",
+                other
+            )),
+        }
+    }
+}
+
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub app_port: u16,
+    pub application: AppSettings,
+}
+
+#[derive(serde::Deserialize)]
+pub struct AppSettings {
+    pub host: String,
+    pub port: u16,
 }
 
 #[derive(serde::Deserialize)]
@@ -14,13 +66,6 @@ pub struct DatabaseSettings {
     pub port: u16,
     pub host: String,
     pub name: String,
-}
-
-pub fn get_config() -> Result<Settings, config::ConfigError> {
-    let config = config::Config::builder()
-        .add_source(File::with_name("configuration"))
-        .build()?;
-    config.try_deserialize()
 }
 
 impl DatabaseSettings {
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,7 +1,7 @@
 #![warn(clippy::perf)]
 
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use tokio::net::TcpListener;
 use zero2prod::{
     configuration::get_config,
@@ -16,13 +16,19 @@ async fn main() {
     init_subscriber(subscriber);
 
     let configuration = get_config().expect("Could not read configuration");
-    let pg_pool = PgPool::connect(configuration.database.connection_string().expose_secret())
-        .await
+    let pg_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(3))
+        .connect_lazy(configuration.database.connection_string().expose_secret())
         .expect("Could not connect to database");
-    let address = format!("127.0.0.1:{}", configuration.app_port);
-    let listener = TcpListener::bind(address)
+
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+    let listener = TcpListener::bind(address.clone())
         .await
         .expect("Could not bind on port");
 
+    log::info!("app launched, listening on {}", address);
     run(listener, pg_pool).await.unwrap();
 }
